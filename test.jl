85c5c9c2707c8f565444a47a609773b205895ca7
Wen Wei Tseng <sosiristseng@gmail.com>
2026-02-10 20:51:48 +0800

    put getsys in the function

--- test.jl
+++ test.jl
@@ -2,14 +2,14 @@ using ModelingToolkit
 using ModelingToolkit: t_nounits as t, D_nounits as D
 using OrdinaryDiffEq
 
-function main()
+function get_sys(; name=:sys)
     @parameters τ = 3.0 # parameters
     @variables x(t) = 0.0 # dependent variables
     eqs = [D(x) ~ (1 - x) / τ]
-    @time "Build system" @named sys = System(eqs, t)
-    @time "MTKCompile" fol = mtkcompile(sys)
-    @time "Build problem" prob = ODEProblem(fol, [], (0.0, 10.0))
-    @time "Solve problem" sol = solve(prob, Tsit5())
+    return System(eqs, t; name)
 end
 
-main()
+@time "Build system" @named sys = get_sys()
+@time "MTKCompile" fol = mtkcompile(sys)
+@time "Build problem" prob = ODEProblem(fol, [], (0.0, 10.0))
+@time "Solve problem" sol = solve(prob, Tsit5())
