a75c9c9c70b4afc69313edcc82a126162a7ed809
Wen Wei Tseng <sosiristseng@gmail.com>
2026-03-08 15:21:57 +0800

    SIR model

--- test.jl
+++ test.jl
@@ -3,12 +3,16 @@ using ModelingToolkit: t_nounits as t, D_nounits as D
 using OrdinaryDiffEq
 
 function main()
-    @parameters τ = 3.0 # parameters
-    @variables x(t) = 0.0 # dependent variables
-    eqs = [D(x) ~ (1 - x) / τ]
-    @time "Build system" @named sys = System(eqs, t)
-    @time "MTKCompile" fol = mtkcompile(sys)
-    @time "Build problem" prob = ODEProblem(fol, [], (0.0, 10.0))
+    @parameters β=0.2 γ=0.15 N=1000
+    @variables S(t)=0.99*N I(t)=0.01*N R(t)
+    eqs = [
+        D(S) ~ -β * S * I / N,
+        D(I) ~ β * S * I / N - γ * I,
+        R ~ N - S - I
+    ]
+    @time "Build system" @named _sys = System(eqs, t)
+    @time "MTKCompile" sys = mtkcompile(_sys)
+    @time "Build problem" prob = ODEProblem(sys, [], (0.0, 100.0))
     @time "Solve problem" sol = solve(prob, Tsit5())
 end
 
