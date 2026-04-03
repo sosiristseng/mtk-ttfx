59ec95bc1f5f2579943f7093170d06314ec399b8
Wen Wei Tseng <sosiristseng@gmail.com>
2026-04-03 23:03:35 +0800

    Collins toggle switch

--- test.jl
+++ test.jl
@@ -3,17 +3,19 @@ using ModelingToolkit: t_nounits as t, D_nounits as D
 using OrdinaryDiffEq
 
 function main()
-    @parameters β=0.2 γ=0.15 N=1000
-    @variables S(t)=0.99*N I(t)=0.01*N R(t)
+    @parameters a1=3 a2=2.5 β=4 γ=4
+    @variables i1(t) i2(t) s1(t)=0.075 s2(t)=2.5
+    hil(x, k) = x / (x + k)
+    hil(x, k, n) = hil(x^n, k^n)
     eqs = [
-        D(S) ~ -β * S * I / N,
-        D(I) ~ β * S * I / N - γ * I,
-        R ~ N - S - I
+        D(s1) ~ a1 * hil(1 + i2, s2, β) - s1,
+        D(s2) ~ a2 * hil(1 + i1, s1, γ) - s2,
+        i1 ~ 10 * (t > 30) * (t < 40),
+        i2 ~ 10 * (t > 10) * (t < 20)
     ]
-    @time "Build system" @named _sys = System(eqs, t)
-    @time "MTKCompile" sys = mtkcompile(_sys)
-    @time "Build problem" prob = ODEProblem(sys, [], (0.0, 100.0))
-    @time "Solve problem" sol = solve(prob, Tsit5())
+    @time "Build system" @named sys = System(eqs, t) |> mtkcompile
+    @time "Build problem" prob = ODEProblem(sys, [], (0.0, 50.0))
+    @time "Solve problem" sol = solve(prob, Tsit5(), tstops=[10.0, 20.0, 30.0, 40.0])
 end
 
 main()
